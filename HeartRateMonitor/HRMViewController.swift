3f2c72a671f5bd6a245605d69bb78a7ae5d5638b
illia kosvin <kosdevkos@gmail.com>
2020-03-23 17:57:28 +0000

    Recording live data to CSV with timeStamp!

--- HeartRateMonitor/HRMViewController.swift
+++ HeartRateMonitor/HRMViewController.swift
@@ -54,10 +54,18 @@ let Back_TR_PWM_IN_CharacteristicCBUUID = CBUUID(string: "c7bd8529-02ff-481f-a8b
 let Front_TR_PWM_OUT_CharacteristicCBUUID = CBUUID(string: "f79f7794-bfd5-455d-a1ae-b97d4b17774a")
 let Back_TR_PWM_OUT_CharacteristicCBUUID = CBUUID(string: "8eb124d6-afaf-4b75-a83f-a14b61b241a4")
 
+let TimeStamp_CharacteristicCBUUID = CBUUID(string: "09dcd516-9e99-46bb-82b6-460838ae7c83")
+let ModeOfOperation_CharacteristicCBUUID = CBUUID(string: "3e26de8c-9fdd-4b39-8b48-a93b90c0a93f")
+let isRecording_CharacteristicCBUUID = CBUUID(string: "84a3a9a6-04d0-440a-ad72-1395572d8924")
+
 
 /// Variables used to store detected characteristics in the required format for buletooth "write without responce" action
 private var Front_TR_PWM_OUT_Characteristic: CBCharacteristic?
 private var Back_TR_PWM_OUT_Characteristic: CBCharacteristic?
+
+private var ModeOfOperation_Characteristic: CBCharacteristic?
+private var isRecording_Characteristic: CBCharacteristic?
+
 
 /// Variables required to generate CSV file
 var taskArr = [Task]()
@@ -87,13 +95,17 @@ class HRMViewController: UIViewController {
     // If Record button is tapped
     @IBAction func dataRecordButtonPushed(_ sender: UIButton) {
       if dataRecordButton.titleLabel!.text == "Record"{
-        dataRecordingEnable = true
+        dataRecordingEnable = 1
+        writeDutyCycleToChar( withCharacteristic: isRecording_Characteristic!, withValue: Data([UInt8(dataRecordingEnable)]))
         dataRecordButton.setTitle("Stop", for: .normal)
       }
       else if dataRecordButton.titleLabel!.text == "Stop"{
-        dataRecordingEnable = false
+        dataRecordingEnable = 0
+        writeDutyCycleToChar( withCharacteristic: isRecording_Characteristic!, withValue: Data([UInt8(dataRecordingEnable)]))
         // After all nessesary data is in the array, create and export a CSV file
         createCSV()
+        //clean the CSV array
+        taskArr = []
         dataRecordButton.setTitle("Record", for: .normal)
       }
     }
@@ -111,16 +123,21 @@ class HRMViewController: UIViewController {
 
   // the fuction sends the ducy cycle chosen by slider to the microcontroller via BLE
   private func writeDutyCycleToChar( withCharacteristic characteristic: CBCharacteristic, withValue value: Data) {
-      
       // Check if it has the write property
       if characteristic.properties.contains(.writeWithoutResponse) && heartRatePeripheral != nil {
-          
           heartRatePeripheral.writeValue(value, for: characteristic, type: .withoutResponse)
-
       }
-      
+  }
+  
+  private func writeIsRecordingToChar( withCharacteristic characteristic: CBCharacteristic, withValue value: Data) {
+      // Check if it has the write property
+      if characteristic.properties.contains(.writeWithoutResponse) && heartRatePeripheral != nil {
+          heartRatePeripheral.writeValue(value, for: characteristic, type: .withoutResponse)
+      }
   }
 
+  
+  
   var centralManager: CBCentralManager!
   var heartRatePeripheral: CBPeripheral!
   
@@ -128,9 +145,9 @@ class HRMViewController: UIViewController {
   var bleDevices: [CBPeripheral] = []
   
   // Flag that enables data recording if the data recording button is tapped
-  var dataRecordingEnable: Bool = false
+  var dataRecordingEnable: UInt8 = 0
   // Variable for CSV data
-  var time: String = "0"
+  var timeStamp: String = "0"
   var objFrontTemp: String = "0"
   var objBackTemp: String = "0"
   var ambFrontTemp: String = "0"
@@ -267,48 +284,60 @@ extension HRMViewController: CBPeripheralDelegate {
         peripheral.setNotifyValue(true, for: characteristic)
       }
       else if characteristic.uuid == Front_TR_PWM_OUT_CharacteristicCBUUID {
-          print("Green LED characteristic found")
-          
+          print("Front_TR_PWM_OUT_Characteristic characteristic found")
           // Set the characteristic
           Front_TR_PWM_OUT_Characteristic = characteristic
-          
           // Unmask green slider
           //greenSlider.isEnabled = true
       } else if characteristic.uuid == Back_TR_PWM_OUT_CharacteristicCBUUID {
-          print("Blue LED characteristic found");
-          
+          print("Back_TR_PWM_OUT_Characteristic characteristic found");
           // Set the characteristic
           Back_TR_PWM_OUT_Characteristic = characteristic
-          
           // Unmask blue slider
          // blueSlider.isEnabled = true
-    }
+      } else if characteristic.uuid == isRecording_CharacteristicCBUUID {
+             print("isRecording_Characteristic characteristic found");
+             // Set the characteristic
+             isRecording_Characteristic = characteristic
+       }else if characteristic.uuid == ModeOfOperation_CharacteristicCBUUID {
+             print("ModeOfOperation_Characteristic characteristic found");
+             // Set the characteristic
+             ModeOfOperation_Characteristic = characteristic
+       }
+      
   }
   }
 
   func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
     switch characteristic.uuid {
+      case TimeStamp_CharacteristicCBUUID:
+        let gotTime = GetTime(from: characteristic)
+        timeStamp = gotTime
+        
+        // If data recording is enabled, append new reading to the array for CSV
+        // Since all data is sent at the same time, all other values are updated as well
+        if dataRecordingEnable == 1 {
+          // Creating a NEW varible that will be appended to an array
+          task = Task()
+          // assiging new readings to that variable
+          task.time = timeStamp
+          task.objFrontTemp = objFrontTemp
+          task.objBackTemp = objBackTemp
+          task.ambFrontTemp = ambFrontTemp
+          task.ambBackTemp = ambBackTemp
+          task.dutyCycleFront = dutyCycleFront
+          task.dutyCycleBack = dutyCycleBack
+          // Appending that new variable to the array
+          taskArr.append(task!)
+        }
+        print("timeStamp is \(timeStamp)")
     case ambient_VDD_CharacteristicCBUUID:
       let ambient_t_VDD = GetTemperature(from: characteristic)
+      // updating a label on the screen
       frontAmbientTemp.text = ambient_t_VDD
+      // updating a global variable for later use in CSV
       ambFrontTemp = ambient_t_VDD
-
-      // If data recording is enabled, append new reading to the array for CSV
-      if dataRecordingEnable == true {
-        // Creating a NEW varible that will be appended to an array
-        task = Task()
-        // assiging new readings to that variable
-        task.time = time
-        task.objFrontTemp = objFrontTemp
-        task.objBackTemp = objBackTemp
-        task.ambFrontTemp = ambFrontTemp
-        task.ambBackTemp = ambBackTemp
-        task.dutyCycleFront = dutyCycleFront
-        task.dutyCycleBack = dutyCycleBack
-        // Appending that new variable to the array
-        taskArr.append(task!)
-      }
-      
+    // Rest are like case 1
     case object_VDD_CharacteristicCBUUID:
       let object_t_VDD = GetTemperature(from: characteristic)
       frontObjectTemp.text = object_t_VDD
@@ -344,10 +373,10 @@ extension HRMViewController: CBPeripheralDelegate {
     let byteArray = [UInt8](characteristicData)
 
     
-    let MBS:UInt8 = UInt8(byteArray[0])  // gets received MSB bits
-    let LBS:UInt8 = UInt8(byteArray[1])  // gets received LSB bits
+    let MSB:UInt8 = UInt8(byteArray[0])  // gets received MSB bits
+    let LSB:UInt8 = UInt8(byteArray[1])  // gets received LSB bits
     let decimalPoints:UInt8 = UInt8(byteArray[2]) // gets received decimal point bits
-    let integer:Int16 = Int16( (MBS << 8) | LBS)  // combines MBS and LBS into integer
+    let integer:Int16 = Int16( (MSB << 8) | LSB)  // combines MBS and LBS into integer
     let resultString:String = "\(String(integer)).\(String(decimalPoints))" //conv to str
 
     return resultString
@@ -357,8 +386,26 @@ extension HRMViewController: CBPeripheralDelegate {
       guard let characteristicData = characteristic.value else { return "error" }
       let byteArray = [UInt8](characteristicData)
       let duty:UInt8 = UInt8(byteArray[0])
-      let resultString:String = "\(String(duty))%" //conv to str
+      let resultString:String = "\(String(duty))" //conv to str
 
+      return resultString
+  }
+  
+    private func GetTime(from characteristic: CBCharacteristic) -> String {
+      guard let characteristicData = characteristic.value else { return "error" }
+      let byteArray = [UInt8](characteristicData)
+      
+      let MSB3:UInt32 = UInt32(byteArray[0])  //
+      let BS2:UInt32 = UInt32(byteArray[1])  //
+      let BS1:UInt32 = UInt32(byteArray[2]) //
+      let LSB0:UInt32 = UInt32(byteArray[3]) //
+      print("MSB3 is \(MSB3)")
+      print("BS2 is \(BS2)")
+      print("BS1 is \(BS1)")
+      print("LSB0 is \(LSB0)")
+      let integer:UInt32 = UInt32( (MSB3 << 24) | (BS2 << 16) | (BS1 << 8) | LSB0)
+      print("integer is \(integer)")
+      let resultString:String = "\(String(integer))" //conv to str
       return resultString
   }
 }
